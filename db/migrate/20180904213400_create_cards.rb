29820c543fa35c7ec36debaa04de9d2a0daa74bd
jlmack13 <jennimacklin@gmail.com>
2018-09-18 12:38:01 -0400

    added to card migration

--- db/migrate/20180904213400_create_cards.rb
+++ db/migrate/20180904213400_create_cards.rb
@@ -1,7 +1,11 @@
 class CreateCards < ActiveRecord::Migration[5.1]
   def change
     create_table :cards do |t|
-
+      t.string :name
+      t.string :class
+      t.string :set
+      t.string :rarity
+      t.integer :mana_cost
       t.timestamps
     end
   end
